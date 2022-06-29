af3c7a029237b7b44f656c2e87b07b8b47158e36
jeni00 <106555289+jeni00@users.noreply.github.com>
2022-06-28 23:46:59 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,100 @@
 
-let channelName = "welcomePage";
+var cursors = [];
 
-let answerInput 
+// variable used for incoming messages
+var x;
+var y;
+var r;
+var g;
+var b;
+var who;  
 
+let channelName = "snowFlake";
+
+// variables from the previous page
+var url = new URL(window.location.href);
+
+// printing out the values so that we know what is going on. 
+console.log(you);
+
+createServer(you); // creating our pubnub server with our name.
 
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
 
-    answerInput = createInput();
-    answerInput.style('font-size', '30px');
-    answerInput.position(windowWidth/3, 325);
+    // listen for messages coming through the subcription feed on this specific channel. 
+    dataServer.addListener({ message: readIncoming});
+    dataServer.subscribe({ channels: [channelName] });
 
-    submitButton = createButton("Enter Site");
-    submitButton.position(windowWidth/3, 650);
-    submitButton.style('font-size', '30px');
-
-    var textArray = [
-      'Durian is the best fruit in the world.ogg',
-      'Fries must come with salt and ketchup.ogg',
-      'Cinnamon is the worst food I had.ogg',
-
-  ];
-  var randomNumber = Math.floor(Math.random()*textArray.length);
+    // create a new JSON object to store the mouse position, colours, and name of the user from the previous page
+    new allCursors(mouseX, mouseY);
   
   }
   
 function draw() {
+
   background(0,0,0);
 
-  textSize(60);
+  sendTheMessage(); 
 
-  textAlign(CENTER);
-
-  text("Welcome! Please make comments to the following statement.", windowWidth/2, 100);
-
-  textSize(30);
-  textAlign(LEFT);
-  text(randomNumber, windowWidth/3, 300);
-
-
-  // on submit enter the information
-  submitButton.mousePressed(sendTheMessage);
-
-}
- 
-function sendTheMessage() {
-
-  // check to see if they enter their name
-  if (answerInput.value() != "") { 
-    // if they did, save their name to the variable "you"
-    you = answerInput.value();
-    // load a new page when you press submit
-    window.location.href = "/../_pageTwo/index.html?you="+you+"&r="+redVal+"&g="+greenVal+"&b="+blueVal; 
+  for (let i = 0; i < cursors.length; i++) { // loop through all the cursors and show them on the page
+    textSize(20);
+    textAlign(CENTER);
+    text(cursors[i].who, cursors[i].x, cursors[i].y+5);
+  
   }
+}
+  
+  // PubNub logic below
+function sendTheMessage() {
+  // Send Data to the server to draw it in all other canvases
+
+  dataServer.subscribe({
+    channel: channelName,
+    message: {
+      x: mouseX,
+      y: mouseY,
+    },
+  });
+}
+
+function readIncoming(inMessage) {
+  // when new data comes in it triggers this function,
+  // we call this function in the setup
+
+  /*since an App can have many channels, we ensure that we are listening
+  to the correct channel */
+
+  if (inMessage.channel == channelName) {
+
+   x = inMessage.message.x; // get the mouseX value from the other people
+   y = inMessage.message.y; // get the mouseY value from the other people
+   who = inMessage.publisher; // who sent the message
+
+  //console.log(inMessage); //logging for information
+
+   let newinput = true; // we are checking to see if this person who sent the message is already on the page. 
+
+      for(let i = 0; i<cursors.length;i++) { // loop through all the IDs that have sent us messages before
+        if(who==cursors[i].who) { // if who is already in our array, update the x & y values
+          cursors[i].x = x;
+          cursors[i].y = y;
+          newinput = false;    // set the boolean to false since this is not a new user
+        }
+      }
+      if(newinput) { // if this is a new user, create a new JSON object that we add to our array
+        cursors.push(new allCursors(x,y,r,g,b,who));
+      }
+  }
+}
+function allCursors(x,y,who){ // creates a new JSON object for us
  
+  this.x = x; // this is shorthand for saying "this object"
+  this.y = y;
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  this.who = who;
 
 }
